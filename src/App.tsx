9f0a1df78776c518f093da4237a9004f541af83a
Anton Strogonoff <anton@strogonoff.name>
2021-10-13 14:25:36 +0700

    refactor: top-bar, makeApp

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,15 @@
-/** @jsx jsx */
-
-import React from 'react'
-import { jsx, css } from '@emotion/react'
-import { Root, Routes } from 'react-static'
-import { Router, Link } from '@reach/router'
-import { Helmet } from 'react-helmet'
-import globalStyle, { Spinner } from './common/GlobalStyle'
-import { BORDER_RADIUS_REM, BIG_SCREEN_BREAKPOINT_PX } from './common/constants'
+import makeApp from './common/makeApp'
 
 
-function App() {
-  return (
-    <Root>
-      {globalStyle}
-
-      <Helmet>
-        <title>Paneron extension directory</title>
-        <meta charSet="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Helmet>
-
-      <h1 css={css`
-          margin: 0;
-
-          @media screen and (min-width: ${BIG_SCREEN_BREAKPOINT_PX}px) {
-            margin: 0 auto;
-            width: 80vw;
-            border-radius: 0 0 ${BORDER_RADIUS_REM}rem ${BORDER_RADIUS_REM}rem;
-          }
-
-          overflow: hidden;
-          align-self: stretch;
-          background: rgba(0, 0, 0, 0.1);
-          font-size: 90%;
-          color: rgba(255, 255, 255, 0.7);
-          display: flex;
-          flex-flow: row nowrap;
-
-          & > * {
-            padding: .5rem 1rem .5rem 1rem;
-          }
-        `}>
-        <span css={css`font-weight: 400`}>Paneron</span>
-        <Link
-            css={css`font-weight: 800; background: rgba(0, 0, 0, 0.2); color: white;`}
-            to="/">
-          Extensions
-        </Link>
-      </h1>
-
-      <React.Suspense fallback={<Spinner />}>
-        <Router>
-          <Routes path="*" />
-        </Router>
-      </React.Suspense>
-    </Root>
-  )
-}
-
-export default App
+export default makeApp({
+  title: "Paneron extension directory",
+  topBarIsHeader: true,
+  topLinks: [{
+    text: "Paneron",
+    target: "https://paneron.org/",
+    external: true,
+  }, {
+    text: "Extensions",
+    target: "/",
+  }],
+});
